b7e599346a85a595f827140b8a5331c018599679
김혜은 <84231143+hyeesw@users.noreply.github.com>
2023-05-10 20:17:32 +0900

    Add files via upload

---
+++ haedam/ksabi.js
@@ -0,0 +1,79 @@
+var ksabi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint8",
+        name: "goodsType",
+        type: "uint8",
+      },
+    ],
+    name: "buy",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "checkSoldout",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getMyGoods",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint8",
+        name: "goodsType",
+        type: "uint8",
+      },
+    ],
+    name: "getNumStock",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getProfits",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "kill",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
